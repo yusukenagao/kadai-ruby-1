c1be61b2a41aede589303c280e4c026de3aa19d3
yusuke.nagao <y.n.227@icloud.com>
2021-09-20 08:29:44 +0000

    kadai_ruby_1

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -2,7 +2,5 @@ family_name = "田中"
 last_name = "太郎"
 name = family_name + last_name
 age = 25
-str1 = "です。"
-str2 = "歳です。"
-puts name + str1 + age.to_s + str2
+puts name + "です。" + age.to_s + "歳です。"
 
